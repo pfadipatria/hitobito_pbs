dd3b35f07fecfc13c676c7e93851de063691eb96
Cedric Walker <cedric.walker@students.unibe.ch>
2014-03-30 15:20:38 +0200

    changed implemetation of Corps to extend Abteilung

--- app/models/group/corps.rb
+++ app/models/group/corps.rb
@@ -1,4 +1,7 @@
-class Group::Corps < Group
+# encoding: utf-8
+# extends class Atbteilung because Corps is an Abteilung with additional Features.
+
+class Group::Corps < Group::Abteilung
  
  self.layer = true
 
@@ -8,161 +11,26 @@ class Group::Corps < Group
                   
   children Group::Abteilung
 
+
   ### ROLES
-  
+
   class Corpsleitung < ::Role
     self.permissions = [:layer_full, :contact_data]
   end
-  
+
   class CorpsleitungStv < ::Role
     self.permissions = [:layer_full, :contact_data]
   end
 
-  class Abteilungsleitung < ::Role
-    self.permissions = [:layer_read, :contact_data]
-  end
-
-  class AbteilungsleitungStv < ::Role
-    self.permissions = [:layer_read, :contact_data]
-  end
-
-  class Adressverwaltung < ::Role
-    self.permissions = [:layer_full]
-  end
-
-  class Beisitz < ::Role
+ class MitgliedCorpsrat < ::Role
     self.permissions = [:group_read]
   end
-
-  class Coach < ::Role
-    self.permissions = [:layer_read, :contact_data]
-  end
-
-  class Ehrenmitglied < ::Role
-    self.permissions = []
-    self.kind = :passive
-  end
-
-  class Heimverwaltung < ::Role
-    self.permissions = [:group_read, :contact_data]
-  end
-
-  class Kassier < ::Role
-    self.permissions = [:layer_read, :contact_data]
-  end
-
-  class Materialwart < ::Role
-    self.permissions = [:group_read]
-  end
-
-  class Passivmitglied < ::Role
-    self.permissions = []
-    self.kind = :passive
-  end
-
-  class Praeses < ::Role
-    self.permissions = [:group_read]
-  end
-
-  class Praesidium < ::Role
-    self.permissions = [:group_read, :contact_data]
-  end
-
-  class PraesidiumApv < ::Role
-    self.permissions = [:group_read]
-  end
-
-  class Redaktor < ::Role
-    self.permissions = [:layer_read, :contact_data]
-  end
-
-  class Revisor < ::Role
-    self.permissions = [:group_read, :contact_data]
-  end
-
-  class Sekretariat < ::Role
-    self.permissions = [:layer_full, :contact_data]
-  end
-
-  class Spezialfunktion < ::Role
-    self.permissions = [:group_read]
-  end
-
-  class StufenleitungBiber < ::Role
-    self.permissions = [:layer_read]
-  end
-
-  class StufenleitungWoelfe < ::Role
-    self.permissions = [:layer_read]
-  end
-
-  class StufenleitungPfadi < ::Role
-    self.permissions = [:layer_read]
-  end
-
-  class StufenleitungPio < ::Role
-    self.permissions = [:layer_read]
-  end
-
-  class StufenleitungRover < ::Role
-    self.permissions = [:layer_read]
-  end
-
-  class StufenleitungPta < ::Role
-    self.permissions = [:layer_read]
-  end
-
-  class VerantwortungMaterialverkaufsstelle < ::Role
-    self.permissions = [:group_read, :contact_data]
-  end
-
-  class VerantwortungPfadiTrotzAllem < ::Role
-    self.permissions = [:group_read, :contact_data]
-  end
-
-  class VerantwortungPr < ::Role
-    self.permissions = [:group_read, :contact_data]
-  end
-
-  class VizePraesidium < ::Role
-    self.permissions = [:group_read, :contact_data]
-  end
-
-  class Webmaster < ::Role
-    self.permissions = [:group_read, :contact_data]
-  end
-
+  
   roles Corpsleitung,
         CorpsleitungStv,
-        Abteilungsleitung,
-        AbteilungsleitungStv,
-        Sekretariat,
-        Adressverwaltung,
-        Praesidium,
-        VizePraesidium,
-        PraesidiumApv,
-        Praeses,
-        Beisitz,
+        MitgliedCorpsrat
 
-        StufenleitungBiber,
-        StufenleitungWoelfe,
-        StufenleitungPfadi,
-        StufenleitungPio,
-        StufenleitungRover,
-        StufenleitungPta,
+    
 
-        Kassier,
-        Revisor,
-        Redaktor,
-        Webmaster,
-        Coach,
 
-        VerantwortungMaterialverkaufsstelle,
-        VerantwortungPfadiTrotzAllem,
-        VerantwortungPr,
-
-        Spezialfunktion,
-
-        Ehrenmitglied,
-        Passivmitglied
 end
