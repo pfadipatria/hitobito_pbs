228dc9048df8948b7a49a6068479f146d3b56eca
Raffael Hertle <raffaelhertle@windowslive.com>
2015-04-20 13:07:11 +0200

    Revert "DefaultValuesFixed"
    This reverts commit 5d12d162183049ea5be1c779283fbc83c64c2960.

--- db/migrate/20131211121712_add_pfadi_attributes.rb
+++ db/migrate/20131211121712_add_pfadi_attributes.rb
@@ -7,13 +7,13 @@
 
 class AddPfadiAttributes < ActiveRecord::Migration
   def change
-    add_column :people, :salutation, :string, default: "lieber_pfadiname"
+    add_column :people, :salutation, :string
     add_column :people, :title, :string
     add_column :people, :grade_of_school, :integer
     add_column :people, :entry_date, :date
     add_column :people, :leaving_date, :date
     add_column :people, :j_s_number, :string
-    add_column :people, :correspondence_language, :string, limit: 5, default: "de"
+    add_column :people, :correspondence_language, :string, limit: 5
     add_column :people, :brother_and_sisters, :boolean, default: false, null: false
 
     add_column :groups, :pta, :boolean, default: false, null: false
